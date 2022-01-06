1ca0ae3df03f1c3b2b3762791ed19a0223a6c986
“liue <522414928@qq.com>
2022-01-06 12:19:43 +0800

    xinyuuliu

---
+++ kendryte_kpu-standalone_3/.vscode/cmake-server.bat
@@ -0,0 +1,9 @@
+cd "C:/Users/xinyuuliu/Desktop/kendryte/updater.beta.20190604.4.win32/KendryteIDE/LocalPackage/cmake/bin"
+SET Path=c:\Users\xinyuuliu\Desktop\kendryte\updater.beta.20190604.4.win32\KendryteIDE\bin;C:\Users\xinyuuliu\Desktop\kendryte\updater.beta.20190604.4.win32\KendryteIDE\LocalPackage\toolchain\bin;C:\Users\xinyuuliu\Desktop\kendryte\updater.beta.20190604.4.win32\KendryteIDE\LocalPackage\cmake\bin;C:\Users\xinyuuliu\Desktop\kendryte\updater.beta.20190604.4.win32\KendryteIDE\LocalPackage\jlink;C:\Users\xinyuuliu\Desktop\kendryte\updater.beta.20190604.4.win32\KendryteIDE\LocalPackage\clang-format\bin;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0;C:\WINDOWS\System32\OpenSSH
+SET PYTHONHOME=C:/Users/xinyuuliu/Desktop/kendryte/updater.beta.20190604.4.win32/KendryteIDE/LocalPackage/python2library
+SET PYTHONPATH=C:/Users/xinyuuliu/Desktop/kendryte/updater.beta.20190604.4.win32/KendryteIDE/LocalPackage/python2library;C:/Users/xinyuuliu/Desktop/kendryte/updater.beta.20190604.4.win32/KendryteIDE/LocalPackage/toolchain/share/gdb/python
+SET PYTHONDONTWRITEBYTECODE=yes
+SET CMAKE_MAKE_PROGRAM=make
+SET KENDRYTE_IDE=yes
+SET LANG=en_US.utf-8
+"C:/Users/xinyuuliu/Desktop/kendryte/updater.beta.20190604.4.win32/KendryteIDE/LocalPackage/cmake/bin/cmake.exe" -E server --experimental --pipe=\\?\pipe\kide.cmake_server_pipe.1641440043930
